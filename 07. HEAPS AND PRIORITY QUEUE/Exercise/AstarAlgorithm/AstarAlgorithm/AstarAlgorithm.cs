f1396e1455f3d736c9c4acab9387ce9d69da624d
aquariuz <gpetiaa@gmail.com>
2019-01-30 08:49:13 +0200

    arranged some files

---
+++ 07. HEAPS AND PRIORITY QUEUE/Exercise/AstarAlgorithm/AstarAlgorithm/AstarAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AstarAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
